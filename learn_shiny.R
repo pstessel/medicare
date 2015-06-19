332d8aceb75cf0b72f2a3500ca90a4030955d83c
pstessel <pstessel@gmail.com>
2015-06-19 15:53:03 -0400

    created censusVis

--- learn_shiny.R
+++ learn_shiny.R
@@ -1,5 +1,11 @@
 setwd("/Volumes/HD2/Users/pstessel/Documents/Git_Repos/medicare")
 library(shiny)
 
-runApp("censusVis", display.mode="showcase")
+counties <- readRDS("census-app/data/counties.rds")
+head(counties)
 
+library(maps)
+library(mapproj)
+source("census-app/helpers.R")
+counties <- readRDS("census-app/data/counties.rds")
+percent_map(counties$white, "darkgreen", "% white")
